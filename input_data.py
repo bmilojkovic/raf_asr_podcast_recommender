eda666bbe20c60cfcd752577b5babb3ef1a0d4ad
Bogdan Bakarec <bbakarec16@raf.rs>
2019-10-15 20:15:07 +0200

    Initial commit, added requirements, added preparation of input data for CNN.

---
+++ input_data.py
@@ -0,0 +1,107 @@
+import numpy
+import scipy.io.wavfile
+import matplotlib.pyplot as plt
+from scipy.fftpack import dct
+
+# Parameters
+pre_emphasis = 0.97
+frame_size = 0.025
+frame_stride = 0.01
+NFFT = 512
+num_ceps = 12 # Hyper-parameter for CNN
+cep_lifter = 22
+nfilt = 40
+
+def prep_input():
+    
+    # Raw signal
+    sample_rate, signal = scipy.io.wavfile.read('0_jackson_0.wav')  # File assumed to be in the same directory
+
+
+    # Emphasized signal (pre-filtering)
+    emphasized_signal = numpy.append(signal[0], signal[1:] - pre_emphasis * signal[:-1])
+
+
+    # Splitting singla into frames
+    frame_length, frame_step = frame_size * sample_rate, frame_stride * sample_rate  # Convert from seconds to samples
+    signal_length = len(emphasized_signal)
+    frame_length = int(round(frame_length))
+    frame_step = int(round(frame_step))
+    num_frames = int(numpy.ceil(float(numpy.abs(signal_length - frame_length)) / frame_step))  # Make sure that we have at least 1 frame
+
+    pad_signal_length = num_frames * frame_step + frame_length
+    z = numpy.zeros((pad_signal_length - signal_length))
+    pad_signal = numpy.append(emphasized_signal, z) # Pad Signal to make sure that all frames have equal number of samples without truncating any samples from the original signal
+
+    indices = numpy.tile(numpy.arange(0, frame_length), (num_frames, 1)) + numpy.tile(numpy.arange(0, num_frames * frame_step, frame_step), (frame_length, 1)).T
+    frames = pad_signal[indices.astype(numpy.int32, copy=False)]
+
+
+    # Window function
+    frames *= numpy.hamming(frame_length)
+
+
+    # Fourier-Transform and Power Spectrum
+    mag_frames = numpy.absolute(numpy.fft.rfft(frames, NFFT))  # Magnitude of the FFT
+    pow_frames = ((1.0 / NFFT) * ((mag_frames) ** 2))  # Power Spectrum
+
+
+    # Filter banks
+    low_freq_mel = 0
+    high_freq_mel = (2595 * numpy.log10(1 + (sample_rate / 2) / 700))  # Convert Hz to Mel
+    mel_points = numpy.linspace(low_freq_mel, high_freq_mel, nfilt + 2)  # Equally spaced in Mel scale
+    hz_points = (700 * (10**(mel_points / 2595) - 1))  # Convert Mel to Hz
+    bin = numpy.floor((NFFT + 1) * hz_points / sample_rate)
+
+    fbank = numpy.zeros((nfilt, int(numpy.floor(NFFT / 2 + 1))))
+    for m in range(1, nfilt + 1):
+        f_m_minus = int(bin[m - 1])   # left
+        f_m = int(bin[m])             # center
+        f_m_plus = int(bin[m + 1])    # right
+
+        for k in range(f_m_minus, f_m):
+            fbank[m - 1, k] = (k - bin[m - 1]) / (bin[m] - bin[m - 1])
+        for k in range(f_m, f_m_plus):
+            fbank[m - 1, k] = (bin[m + 1] - k) / (bin[m + 1] - bin[m])
+    filter_banks = numpy.dot(pow_frames, fbank.T)
+    filter_banks = numpy.where(filter_banks == 0, numpy.finfo(float).eps, filter_banks)  # Numerical Stability
+    filter_banks = 20 * numpy.log10(filter_banks)  # dB
+
+    # MFSC
+    mfsc = filter_banks[:, 1 : (num_ceps + 1)] # Keep 2-13
+    (nframes, ncoeff) = mfsc.shape
+    n = numpy.arange(ncoeff)
+    lift = 1 + (cep_lifter / 2) * numpy.sin(numpy.pi * n / cep_lifter)
+    mfsc *= lift  #*
+
+
+    # Mean normalization
+    mfsc -= (numpy.mean(mfsc, axis=0) + 1e-8)
+
+
+    # Delta and delta-delta
+    delta_mfsc = delta(mfsc, 2)
+    delta_delta_mfsc = delta(delta_mfsc, 2)
+
+
+    # Final CNN input
+    cnn_input = (mfsc, delta_mfsc, delta_delta_mfsc)
+
+    return cnn_input
+
+
+def delta(feat, N):
+    """Compute delta features from a feature vector sequence.
+    :param feat: A numpy array of size (NUMFRAMES by number of features) containing features. Each row holds 1 feature vector.
+    :param N: For each frame, calculate delta features based on preceding and following N frames
+    :returns: A numpy array of size (NUMFRAMES by number of features) containing delta features. Each row holds 1 delta feature vector.
+    """
+    if N < 1:
+        raise ValueError('N must be an integer >= 1')
+    NUMFRAMES = len(feat)
+    denominator = 2 * sum([i**2 for i in range(1, N+1)])
+    delta_feat = numpy.empty_like(feat)
+    padded = numpy.pad(feat, ((N, N), (0, 0)), mode='edge')   # padded version of feat
+    for t in range(NUMFRAMES):
+        delta_feat[t] = numpy.dot(numpy.arange(-N, N+1), padded[t : t+2*N+1]) / denominator   # [t : t+2*N+1] == [(N+t)-N : (N+t)+N+1]
+    return delta_feat
