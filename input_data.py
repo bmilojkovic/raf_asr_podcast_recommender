ca577c9abb3245a840170cfa79da2f285967d713
mmilunovic <milunovicmilos@gmail.com>
2019-10-21 20:06:35 +0200

    gmm-hmm added, data pushed for testing

--- input_data.py
+++ input_data.py
@@ -32,7 +32,7 @@ def prep_input(wave, nfft=NFFT):
     frame_length, frame_step = frame_size * sample_rate, frame_stride * sample_rate  # Convert from seconds to samples
     signal_length = len(emphasized_signal)
     frame_length = int(round(frame_length))
-    frame_step = i nt(round(frame_step))
+    frame_step = int(round(frame_step))
     num_frames = int(numpy.ceil(float(numpy.abs(signal_length - frame_length)) / frame_step))  # Make sure that we have at least 1 frame
 
     pad_signal_length = num_frames * frame_step + frame_length
